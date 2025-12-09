4680a6ea94964fe1d477c4e60f294662dae48bb8
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-12-08 23:40:47 -0600

    redo ratelimit

--- utils/rateLimit.js
+++ utils/rateLimit.js
@@ -4,9 +4,7 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   message: 'Too many requests from this IP, please try again later.',
-  standardHeaders: true,
-  legacyHeaders: false,
-})
+});
 
 module.exports = limiter;
 
