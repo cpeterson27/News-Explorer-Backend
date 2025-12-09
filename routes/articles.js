32ba9ce4b42e1403c1e269ed3228635f1cf46b61
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-12-08 19:59:05 -0600

    change routes in articles

--- routes/articles.js
+++ routes/articles.js
@@ -3,8 +3,8 @@ const auth = require('../middleware/auth');
 const { validateArticle } = require("../middleware/validation");
 const { getArticles, saveArticle, deleteArticle } = require("../controller/articles");
 
-articleRoute.get('/articles',auth, getArticles);
-articleRoute.post('/articles', auth, validateArticle, saveArticle);
-articleRoute.delete('/articles/:id', auth, deleteArticle);
+articleRoute.get('/',auth, getArticles);
+articleRoute.post('/', auth, validateArticle, saveArticle);
+articleRoute.delete('/:id', auth, deleteArticle);
 
 module.exports = articleRoute;
