bcda8cddf68d7db1692bd24f547bf0e20ba56353
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-12-08 20:38:05 -0600

    Fix news route

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/search', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const { q, from, to, sortBy = 'publishedAt' } = req.query;
 
